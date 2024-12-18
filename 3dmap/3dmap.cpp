31c7ba17804629e84c01ed0a00add315439ca07c
rachit <rachit.bhi004@gmail.com>
2024-12-18 16:03:08 +0530

    add floor, walls and celilings

--- 3dmap/3dmap.cpp
+++ 3dmap/3dmap.cpp
@@ -1,20 +1,109 @@
-// 3dmap.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+#include <Windows.h>
+using namespace std;
 
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 0.0f;
+float fPlayerY = 0.0f;
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+float fFov = 3.14 / 4.0;
+float fDepth = 16.0f;
 int main()
 {
-    std::cout << "Hello World!\n";
+    wchar_t* screen = new wchar_t[nScreenHeight*nScreenWidth];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    wstring map;
+
+    map += L"################";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"################";
+
+
+    while (true) {
+
+        for (int x = 0; x < nScreenWidth; x++) {
+            
+            float fRayAngle = (fPlayerA - fFov / 2.0f)
+                                + ((float)x / (float)nScreenWidth)
+                                * fFov;
+
+            float fDistanceToWall = 0;
+            bool bHitWall = false;
+
+            float fEyeX = sinf(fRayAngle);
+            float fEyeY = cosf(fRayAngle);
+
+            while (!bHitWall && fDistanceToWall < fDepth) {
+
+                fDistanceToWall += 0.1f;
+
+                int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+                int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+            
+            
+                if (nTestX < 0 || nTestY >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) {
+                    bHitWall = true;
+                    fDistanceToWall = fDepth;
+                }
+                else {
+                
+                    if (map[nTestY * nMapWidth + nTestX] == '#') {
+                        
+                        bHitWall = true;
+
+                    }
+                
+                }
+            }
+       
+            int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+            int nFloor = nScreenHeight - nCeiling;
+            
+            for (int y = 0; y < nScreenHeight; y++) {
+            
+                if (y < nCeiling) {
+                    screen[y * nScreenWidth + x] = ' ';
+                }
+                else if(y > nCeiling && y <= nFloor) {
+                    screen[y * nScreenWidth + x] = '@';
+                }
+                else {
+                    screen[y * nScreenWidth + x] = ' ';
+                }
+            }
+
+        }
+        
+
+        screen[nScreenHeight * nScreenWidth - 1] = '\0';
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+    }
+
+
+    //std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
